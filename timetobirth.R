c05b0307ef6e8aa18549b5ea17e1bd8381849368
Sebastien DIAZ <sebastien.diaz@gmail.com>
2016-03-06 18:35:01 +0100

    add

--- timetobirth.R
+++ timetobirth.R
@@ -51,3 +51,56 @@ reg<-survreg(Surv(d2g, indic) ~ fumH, data = d2g,
              dist="weibull")
 curve(exp(-(exp(-reg$coef[1]) * x)^(1/reg$scale)),
       col="blue", add=TRUE)
+
+d2g$bmiFQuant <- cut(d2g$bmiF,breaks=quantile(d2g$bmiF),include.lowest=TRUE)
+d2g$bmiHQuant <- cut(d2g$bmiH,breaks=quantile(d2g$bmiH),include.lowest=TRUE)
+levels(d2g$bmiFQuant)
+levels(d2g$bmiHQuant)
+
+outt=survfit(Surv(d2g,indic) ~bmiFQuant,data=d2g)
+plot(outt,lty=1:4,col=1:4,xlab="years",ylab="Probability, KM") ### plusieurs type de lignes
+legend(3,0.4,levels(d2g$bmiFQuant),lty=1:4,col=1:4)
+title(main="Melanoma data")
+
+outt=survfit(Surv(d2g,indic) ~bmiHQuant,data=d2g)
+plot(outt,lty=1:4,col=1:4,xlab="years",ylab="Probability, KM") ### plusieurs type de lignes
+legend(3,0.4,levels(d2g$bmiHQuant),lty=1:4,col=1:4)
+title(main="Melanoma data")
+
+survdiff(Surv(d2g,indic) ~bmiFQuant,data=d2g)
+survdiff(Surv(d2g,indic) ~bmiHQuant,data=d2g)
+
+d2g$alcFQuant <- cut(d2g$alcF,breaks=unique(quantile(d2g$alcF)),include.lowest=TRUE)
+d2g$alcHQuant <- cut(d2g$alcH,breaks=quantile(d2g$alcH),include.lowest=TRUE)
+levels(d2g$alcFQuant)
+levels(d2g$alcHQuant)
+
+outt=survfit(Surv(d2g,indic) ~alcFQuant,data=d2g)
+plot(outt,lty=1:4,col=1:4,xlab="years",ylab="Probability, KM") ### plusieurs type de lignes
+legend(3,0.4,levels(d2g$alcFQuant),lty=1:4,col=1:4)
+title(main="Melanoma data")
+
+outt=survfit(Surv(d2g,indic) ~alcHQuant,data=d2g)
+plot(outt,lty=1:4,col=1:4,xlab="years",ylab="Probability, KM") ### plusieurs type de lignes
+legend(3,0.4,levels(d2g$alcHQuant),lty=1:4,col=1:4)
+title(main="Melanoma data")
+
+survdiff(Surv(d2g,indic) ~alcFQuant,data=d2g)
+survdiff(Surv(d2g,indic) ~alcHQuant,data=d2g)
+d2g$sperm<-as.numeric(as.character(d2g$sperm))
+d2gSperm<-d2g[complete.cases(d2g[,12]),]
+
+d2gSperm$sperm<-as.numeric(as.character(d2gSperm$sperm))
+d2gSperm$spermQuant <- cut(d2gSperm$sperm,breaks=unique(quantile(d2gSperm$sperm)),include.lowest=TRUE)
+
+levels(d2gSperm$spermQuant)
+
+
+outt=survfit(Surv(d2g,indic) ~spermQuant,data=d2gSperm)
+plot(outt,lty=1:4,col=1:4,xlab="years",ylab="Probability, KM") ### plusieurs type de lignes
+legend(3,0.4,levels(d2gSperm$spermQuant),lty=1:4,col=1:4)
+title(main="Melanoma data")
+
+
+survdiff(Surv(d2g,indic) ~spermQuant,data=d2gSperm)
+
